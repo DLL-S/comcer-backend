e23ffbeafffa6bb3ae01298fe94c59305aaf0d9d
leonardopereira10 <leonardopereira10@gmail.com>
2022-01-24 23:34:52 -0300

    Update IServicoDePedido.cs

--- DLLS.Comcer/DLLS.Comcer.Interfaces/InterfacesDeServicos/IServicoDePedido.cs
+++ DLLS.Comcer/DLLS.Comcer.Interfaces/InterfacesDeServicos/IServicoDePedido.cs
@@ -1,8 +1,10 @@
 ﻿using DLLS.Comcer.Interfaces.Modelos;
+using DLLS.Comcer.Utilitarios.Enumeradores;
 
 namespace DLLS.Comcer.Interfaces.InterfacesDeServicos
 {
 	public interface IServicoDePedido : IServicoPadrao<DtoPedido>
 	{
+		DtoSaida<DtoPedido> AtualizeStatus(int codigo, EnumStatusPedido statusPedido);
 	}
 }
