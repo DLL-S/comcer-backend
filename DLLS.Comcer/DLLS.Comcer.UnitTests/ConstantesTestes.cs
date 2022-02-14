9aca6d3175f40f4ea2cac769de9caee13870f1d9
leonardopereira10 <leonardopereira10@gmail.com>
2022-02-14 11:49:23 -0300

    Update ConstantesTestes.cs

--- DLLS.Comcer/DLLS.Comcer.UnitTests/ConstantesTestes.cs
+++ DLLS.Comcer/DLLS.Comcer.UnitTests/ConstantesTestes.cs
@@ -4,7 +4,9 @@
 	{
 		public const string STRING = "Teste";
 		public const int INT = 1;
-		public static readonly byte[] BYTE_ARRAY = { 76, 69, 211 };
+		public static byte[] BYTE_ARRAY {
+			get => new byte[] { 76, 69, 211 };
+		}
 		public const bool BOOL = true;
 		public const decimal DECIMAL = 1.50M;
 	}
